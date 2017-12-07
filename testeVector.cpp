64eb34d7ad2ec9aeeb82b0331f5e9f8c19f3e992
Hemerson-GH <hemersonel@gmail.com>
2017-12-07 20:57:12 -0200

    almost finnality

--- testeVector.cpp
+++ testeVector.cpp
@@ -6,7 +6,19 @@
 
 using namespace std;
 
-vector <vector <int> > GRAPH(100); // constroi um vector de 100 linhas
+struct Aresta {
+	int vertex;
+	int weight;
+	
+	Aresta(int v, int w);
+};
+
+Aresta::Aresta(int v, int w){
+	vertex = v;
+	weight = w;
+}
+
+vector < vector <Aresta> > GRAPH(100); // constroi um vector de 100 linhas};
 
 inline void imprimiCaminho(vector <int> caminho)
 {
@@ -34,6 +46,7 @@ bool nohVizinhoNaoEstaPresenteNoCaminho(int node, vector <int> caminho)
 int encontraCaminho(int partida, int destino)
 {
     vector <int> caminho; // crio um novo vector para armazenar o caminho
+    //~ vector <int> caminho; // crio um novo vector para armazenar o caminho
     caminho.push_back(partida); // adiciono o vertice de partida ao inicio do caminho
     queue < vector <int>> q; // cria uma fila de vector
     q.push(caminho); // coloco o caminho(vector) na fila
@@ -43,11 +56,11 @@ int encontraCaminho(int partida, int destino)
         caminho = q.front(); // caminho recebe primeiro elemento da fila, que antes era o caminho
         q.pop(); // desenfilera a fila 
 		
-		cout << "q.front() => " << caminho[1];
+		//~ cout << "q.front() => " << caminho[1];
 		
         int ultimoNohDoCaminho = caminho[caminho.size()-1]; // ultimonoh recebendo o ultimo elemento do caminho(vector)
 	
-		cout << "ultnohcaminho --> " << ultimoNohDoCaminho << endl;
+		//~ cout << "ultnohcaminho --> " << ultimoNohDoCaminho << endl;
 	
         if(ultimoNohDoCaminho == destino) // compara se ja chegou no destino
         {
@@ -59,18 +72,21 @@ int encontraCaminho(int partida, int destino)
            // imprimiCaminho(caminho);
         } 
         
-        cout << "GRAPH[ultNohDoCam].size --> " << (int)GRAPH[ultimoNohDoCaminho].size() << endl;
+        //~ cout << "GRAPH[ultNohDoCam].size --> " << (int)GRAPH[ultimoNohDoCaminho].size() << endl;
 
        for(int i = 0; i < (int)GRAPH[ultimoNohDoCaminho].size(); i++)
-        {
-            if(nohVizinhoNaoEstaPresenteNoCaminho(GRAPH[ultimoNohDoCaminho][i], caminho))
+       {
+            //if(nohVizinhoNaoEstaPresenteNoCaminho(GRAPH[ultimoNohDoCaminho][i], caminho))
+            if(nohVizinhoNaoEstaPresenteNoCaminho(GRAPH[ultimoNohDoCaminho][i].vertex, caminho))
             {
+                //~ vector <int> new_caminho(caminho.begin(), caminho.end());
                 vector <int> new_caminho(caminho.begin(), caminho.end());
-                new_caminho.push_back(GRAPH[ultimoNohDoCaminho][i]);
+                new_caminho.push_back(GRAPH[ultimoNohDoCaminho][i].vertex);
                 q.push(new_caminho);
             }
         }
     }
+    
     return 1;
 }
 int main()
@@ -79,11 +95,21 @@ int main()
     
     while(T--)
     {
-		GRAPH[1].push_back(2);
-		GRAPH[1].push_back(3);
-		GRAPH[2].push_back(3);
-		GRAPH[2].push_back(4);
-		GRAPH[3].push_back(5);
+		GRAPH[1].push_back( Aresta(2, 10) );
+		GRAPH[2].push_back( Aresta(1, 10) );
+		
+		GRAPH[1].push_back( Aresta(3, 15) );
+		GRAPH[3].push_back( Aresta(1, 15) );
+		
+		GRAPH[2].push_back( Aresta(3, 8) );
+		GRAPH[3].push_back( Aresta(2, 8) );
+		
+		GRAPH[2].push_back( Aresta(4, 1) );
+		GRAPH[4].push_back( Aresta(2, 1) );
+		
+		GRAPH[3].push_back( Aresta(5, 5) );
+		GRAPH[5].push_back( Aresta(3, 5) );
+		
         encontraCaminho(1, 5);
     }
 
