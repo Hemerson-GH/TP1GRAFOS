e84e53804a06a7d3c5d038dea7fc929e0135376b
Hemerson-GH <hemersonel@gmail.com>
2017-12-09 03:28:11 -0200

    read

--- LerArq2.cpp
+++ LerArq2.cpp
@@ -3,68 +3,87 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <time.h>
 #include <cmath>
 
 using namespace std;
 
-//////////////////// FUNCOeS AUXILIAReS ////////////////////
 struct graph{
   int peso;
   int vertix;
   int ancestral;
 };
 
-struct Aresta{
-	int x1, y1, x2, y2;
-	Aresta(int x1, int x2, int y1, int y2);
+struct Cordenadas{
+	int vertex;
+	float x, y, x2, y2;
+	Cordenadas(int vertex, float x, float y);
+	Cordenadas(float x, float y, float x2, float y2);
 };
 
-Aresta::Aresta(int xP1, int xP2, int yP1, int yP2){
-	x1 = xP1;
-	y1 = yP1;
-	x2 = xP2; 
-	y2 = yP2;
-}
-//procurar todas as possibilidades de caminhos
-// funcao paths(matriz, vetices)
+//~ struct Cordenada{
+	//~ float x, y;
+	//~ Cordenada(float x, float y);
+//~ };
 
-int numVertices(vector <Aresta> v)
-{
-	for (int i = 0; i < (int)v.size(); i++)
-	{
-		i--;
-	}	
-	return 1;
+Cordenadas::Cordenadas(int v, float xP1, float yP1){
+	vertex = v;
+	x = xP1;
+	y = yP1;
+}
+Cordenadas::Cordenadas(float xP1, float yP1, float xP2, float yP2){
+	x = xP1;
+	y = yP1;
+	x = xP2;
+	y = yP2;
 }
 
-int addVertice(vector <Aresta> vertices, int x1, int x2)
-{
+vector <Cordenadas> vertices;
+vector <Cordenadas> grafoInteiro;
+
+bool addvertices(int i, float x, float y) {
 	bool repeat = false;
 	
-	for (int i = 0; i < (int)vertices.size() && repeat == false; i++, i++)
-	{
-		if ( (x1 == vertices[i].x1 && x2 == vertices[i].x2))
-		{
-			repeat = true;
-			cout << x1 << " " << x2 << endl;
-			cout << vertices[i].x1 << " " << vertices[i+1].x2 << endl;
-			cout << vertices[i].y1 << " " << vertices[i+1].y2 << endl << endl;
-		}		
-	}	
-	
-	if (!repeat)
-	{
-		//~ vertices.push_back();
-		return 1;
+	if ((int)vertices.size() != 0) {
+		for (int i = 0; i < (int)vertices.size() && repeat == false; i++) {
+			if ( (x == vertices[i].x) && (y == vertices[i].y)) {
+				repeat = true;			
+			}		
+		}	
 	}
 	
-	cout << "NIL" << endl;	
-	return 0;
+	if (repeat == false) {
+		vertices.push_back(Cordenadas(i, x, y));
+		return true;
+	}
+	
+	return false;
 }
 
 int euclideanDistance(int x1, int x2, int y1, int y2){
 	return sqrt( pow(x2-x1, 2) + pow(y2-y1, 2) );
 }
+
+void preencheMat(int** mat){
+	int valor = 0, iterador =  0;
+	for (int i = 0; i < (int)grafoInteiro.size(); i++)
+	{
+		for (int j = 0; j < (int)vertices.size(); j++)
+		{
+			if ( (grafoInteiro[i].x == vertices[j].x && grafoInteiro[i].y == vertices[j].y) ||
+			     (grafoInteiro[i].x == vertices[j].x && grafoInteiro[i].y == vertices[j].y) )
+			{
+				
+				valor = euclideanDistance(grafoInteiro[i].x, grafoInteiro[i].x2, grafoInteiro[i].y, grafoInteiro[i].y2);
+				iterador = (int)vertices[j].vertex;
+				cout << valor << endl;
+				mat[iterador][iterador] = valor;
+			}
+		}	
+	}	
+}
+
+
 
 //~ int main(int argc, char* argv[])
 int main()
@@ -79,84 +98,89 @@ int main()
 	//~ ifstream arq(argv[1]);
 	ifstream arq("exe2.txt");
 	
-	string cases;
+	string cases, linha = "";
 	
-	string ele;
-	
-	string linha = "";
-	
-	vector <Aresta> ligacoes;
-	
-	int x2, y2, x1, y1;
-	
-	string vertexInit[2];
-	//~ int mat[3][4];
-	
-	vector <int> vertex;
-
-	int n = 0;
+	float x2, y2, x1, y1;
+	int i = 1;
 	
 	if (arq.good()) {
 
-		//~ arq >> cases;
 		getline(arq, cases);
-		
-		//~ arq >> vertexInit[0];
-		getline(arq, vertexInit[0]);
-		//~ getline(arq, vertexInit[1]);
-		//~ arq >> vertexInit[1];
-		
-		//~ int espacoEmBranco;
-		//~ arq >> espacoEmBranco;
 		getline(arq, linha);
+		getline(arq, linha);
+		
+		stringstream line(linha);
+		line >> x1 >> y1;
+		vertices.push_back( Cordenadas(i, x1, y1) );		
+		//~ grafoInteiro.push_back( Cordenadas(x1, y1) );		
+		i++;
 		int cont = 0;
 		
 		while(getline(arq, linha) && cont == 0)
 		{
-			//~ if (linha == "" ) {cout << "Parei"; break;}
+			
 			if (linha == "" ) {cont = 1;}
 			
 			stringstream line(linha);
-			//~ ligacoes.push_back(linha);
 			
 			line >> x1 >> y1 >> x2 >> y2;
-			//~ cout << x1 << ' ' << y1 << ' ' << x2 << ' ' << y2 << endl;
 			
-			//~ cout << "distancia " << euclideanDistance(x1, x2, y1, y2) << endl;
-			
-			if(cont == 0) { 
-				ligacoes.push_back( Aresta(x1, x2, y1, y2) );
-				n += addVertice(ligacoes, x1, x2);
-				n += addVertice(ligacoes, y1, y2);
-				
+			if(linha != "" && cont == 0) { 
+				if ( addvertices(i, x1, y1) ) { i++; } 
+				if ( addvertices(i, x2, y2) ) { i++; }
+				grafoInteiro.push_back(Cordenadas(x1, y1, x2, y2));
 			}
 		}
 		
+		int n = (int)vertices.size();
 		
+		int **mat = new int*[n];
+	
+	
+		for (int i = 0; i < n; i++)
+		{
+			mat[i] = new int[n];
+		}
+		
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				mat[i][j] = 0;
+			}			
+		}
+		
+		preencheMat(mat);
+		
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				cout << mat[i][j] << " " << endl;
+			}			
+		}
 
 	} else {
 		cout << "Arquivo corrompido" << endl;
 	}
 	
-	cout << n << endl;
-	
-	for (int i = 0; i < (int)ligacoes.size(); i++)
+	for (int i = 0; i < (int)vertices.size(); i++)
 	{
-		cout << ligacoes[i].x1 << ' ' << ligacoes[i].y1 << ' ' << ligacoes[i].x2 << ' ' << ligacoes[i].y2 << endl;
+		cout << vertices[i].vertex << "\t" << vertices[i].x << "\t" << vertices[i].y << "\t";
+		if (i == 1 || i == 3)
+		{
+			cout << "\n";
+		}
+		
 	}
 	
+	clock_t Ticks[2];
+    Ticks[0] = clock();
+    //O código a ter seu tempo de execução medido ficaria neste ponto.
+    Ticks[1] = clock();
+    double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+    cout << "Tempo gasto: ms." <<  Tempo;
+    getchar();
 	
-	//~ cout << cases << endl;
-	//~ cout << vertexInit[0] << " === " << vertexInit[1] << endl;
-	
-	//~ for (int i = 0; i < 2; i++)
-	//~ {
-		//~ cout << vertexInit[i] << endl;	
-	//~ }	
-	//~ for (int i = 0; i < (int) texto.size(); i++)
-	//~ {
-		//~ cout << texto[i] << ' ';	
-	//~ }	
-
   return 0;
 }
